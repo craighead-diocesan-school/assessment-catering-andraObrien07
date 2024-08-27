c11e58b7639cb5f213b242aff60ffe595967b611
andraObrien07 <comanan09@craighead.school.nz>
2024-08-27 12:44:49 +1200

    comments

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
   import Header from "$lib/Header.svelte";
 
-  // go to the getWalls function and it will send you back the array.
+  // go to the getfood function and it will send you back the array.
   let foodToBuy = getFood();
   // let selected = carsForHire[0];
 
@@ -14,21 +14,27 @@
     // send the array to where this function was called from.
     return foodData.json();
   }
-
+  //array storing the foods that have been added to the menu
   let menu = [];
 
+  //holds the custom name that the user inputs
   let nameMenu = [];
 
+  //adds a specific food to the menu array
   function addFoodToMenu(foodItem) {
     menu = [...menu, foodItem];
+    //sets the selcted to true so that the highlight can run
     foodItem.selected = true;
     foodToBuy = foodToBuy;
   }
 
+  //sets the gst to 15% which is ready to be plussed to each food
   let GST = 0.15;
 
-  function removeFoodFromMenu(index) {
+  //removes food from the menu (dependent on its index)
+  function removeFoodFromMenu(index, foodItem) {
     menu = [...menu.slice(0, index), ...menu.slice(index + 1)];
+    //sets the selected to false to remove the highlight from the food when removed from menu
     foodItem.selected = false;
     foodToBuy = foodToBuy;
   }
@@ -36,22 +42,27 @@
 
 <Header />
 <p>Name your custom menu</p>
+
+<!-- user can input their custom menu name -->
 <input bind:value={nameMenu} />
 <div class="columns">
   <div class="column">
+    <!-- waiting for the food gto be fetched from the array -->
     {#await foodToBuy}
       ...waiting
     {:then foodToBuy}
-      <!-- two diff loops to go through the two diff arrays. fast and nice -->
+      <!-- three diff loops to go through the three diff arrays. breakfast, dinner, and dessert -->
+
+      <!-- looping through the breakfast array and displaying each foodToBuy from that array -->
       {#each foodToBuy.breakfast as foodItem}
+        <!-- highlight the fooitem if the add to menu button has been clicked which changes the selected to true -->
         <div class:highlighted={foodItem.selected}>
           <p>Breakfast</p>
           {foodItem.item}
           {foodItem.description}
-          <!-- {foodItem.selected} -->
           ${foodItem.price}
           {#if addFoodToMenu}
-            <p>Already added to menu</p>
+            <!-- disable if food is already in the menu array  -->
             <button
               disabled={menu.includes(foodItem)}
               on:click={() => {
@@ -66,6 +77,7 @@
         </div>
       {/each}
 
+      <!-- looping through the dinner array and displaying each foodToBuy from that array -->
       {#each foodToBuy.dinner as foodItem}
         <div class:highlighted={foodItem.selected}>
           <p>Dinner</p>
@@ -87,6 +99,7 @@
         </div>
       {/each}
 
+      <!-- looping through the dessert array and displaying each foodToBuy from that array -->
       {#each foodToBuy.dessert as foodItem}
         <div class:highlighted={foodItem.selected}>
           <p>Dessert</p>
